fb335fe95b6e174871c0e718e6577f4d8943fa99
Hardelle Briones <111712687+HardelleBriones@users.noreply.github.com>
2022-12-16 20:55:33 +0800

    Change initial states of speed and distance

--- AliacFuzzyLogic/Form1.Designer.cs
+++ AliacFuzzyLogic/Form1.Designer.cs
@@ -78,7 +78,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 31);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "40";
+            this.textBox1.Text = "80";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
@@ -88,7 +88,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 31);
             this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "40";
+            this.textBox2.Text = "20";
             // 
             // label1
             // 
@@ -201,7 +201,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Aliac Automobile Cruise Control";
+            this.Text = "Briones Brake Control";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
